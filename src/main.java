a5309850885f66cb56d5c9e4951a928d5d4fac50
Alexander Latyshev <imposya0@gmail.com>
2023-01-23 19:36:16 +0300

    new commit 123

--- src/main.java
+++ src/main.java
@@ -2,5 +2,10 @@ public class main {
     public static void main(String[] args) {
         System.out.println("q");
         System.out.println("develop");
+        System.out.println("work1");
+        System.out.println("work2");
+        System.out.println("work3");
+        System.out.println("4");
+        System.out.println("onemore");
     }
 }
